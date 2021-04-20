31e13a8cc3c0a60593d459899c1ccc083e4f1587
Rabbi <alirabbi3112@gmail.com>
2021-04-21 01:12:06 +0600

    Done the taking input form for search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 
 class SearchBar extends React.Component{
+    state = { term : ''}
+
+    onSubmitForm(event){
+        event.preventDefault();
+        console.log(this.state.term)
+    }
+
+
     render(){
         return(
-            <div>
-                <form>
-                    <input type= 'text' />
+            <div className='ui segment'>
+                <form onSubmit = {this.onSubmitForm} className = 'ui form'>
+                    <div className= 'field'>
+                        <label>Image Search</label>
+                       <input type= 'text' 
+                       value = {this.state.term}
+                       onChange={(e)=>this.setState({term: e.target.value})} />
+                    </div>
                 </form>
             </div>
         )
